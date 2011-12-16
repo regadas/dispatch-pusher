7fd9ccca53e842a4e54dc7a098629bbd21825f05
Filipe Regadas <filiperegadas@gmail.com>
2011-12-16 17:11:32 +0000

    add socket_id support and clean a bit

--- src/main/scala/pusher.scala
+++ src/main/scala/pusher.scala
@@ -4,15 +4,17 @@ import dispatch._
 import dispatch.Request._
 import scala.collection.mutable.{ OpenHashMap => OHM }
 
-case class PusherRequest(appId: String, channel: String, key: String, secret: String, authVersion: String = "1.0")(extraParams: OHM[String, String] = OHM())(val data: String) {
+case class PusherRequest(appId: String, channel: String, key: String, secret: String, event: String, val data: String, socketId: Option[String], authVersion: String = "1.0") {
   import Security._
 
-  lazy val innerParams = List[(String, String)](
+  lazy val params = List[(String, String)](
     'auth_key.name -> key,
     'auth_timestamp.name -> (System.currentTimeMillis / 1000).toString,
     'auth_version.name -> authVersion,
-    'body_md5.name -> Security.md5(data))
-  lazy val params = innerParams ++ extraParams
+    'body_md5.name -> Security.md5(data),
+    'name.name -> event) ::: (socketId map {
+      case i => ('socket_id.name -> i) :: Nil
+    } getOrElse Nil)
 
   def asMap()(implicit req: Request) = {
     val toBeHashed = {
@@ -28,8 +30,8 @@ case class PusherRequest(appId: String, channel: String, key: String, secret: St
 }
 
 object PusherRequest {
-  def apply(key: String, secret: String)(appId: String, channel: String)(params: OHM[String, String])(data: String) =
-    new PusherRequest(appId, channel, key, secret)(params)(data)
+  def apply(key: String, secret: String)(appId: String, channel: String)(event: String, data: String, socketId: Option[String]) =
+    new PusherRequest(appId, channel, key, secret, event, data, socketId)
 }
 
 case object Pusher {
--- src/test/scala/pusherspec.scala
+++ src/test/scala/pusherspec.scala
@@ -11,20 +11,27 @@ class PusherSpec extends Specification {
       val key = System.getenv("PUSHER_KEY")
       val secret = System.getenv("PUSHER_SECRET")
       val apiId = System.getenv("PUSHER_APIID")
-      
+
       key must not be empty
       secret must not be empty
       apiId must not be empty
-      
-      val http = new Http
-      val pusher = PusherRequest(key, secret)_
-      val channel = pusher(apiId, "test_channel")
-      val push: Request = channel(OHM("name" -> "my_event"))("hello world")
 
-      val status: Int = http((push >:> identity) {
-        case (status, _, _, _) => status
-      })
-      status must_== 202
+      //setting pusher credentials
+      val pusher = PusherRequest(key, secret)_
+      //specify
+      val channel = pusher(apiId, "test_channel")
+      //specify the event to trigger and the data to be sent ... socket id is optional
+      val pushToMyEvent: Request = channel("my_event", "hello world", None)
+      val pushToYourEvent: Request = channel("your_event", "hello world again", None)
+
+      val http = new Http
+      val requests = List(pushToMyEvent, pushToYourEvent)
+      requests.map { req =>
+        val status :Int = http(( req >:> identity) {
+          case (status, _, _, _) => status
+        })
+        status must_==202
+      }
     }
   }
 }
